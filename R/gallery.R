1e2519292fc3e04ca593b13bdcf3b03c7cbd6acd
BioinfoUSAL <bioinfo@usal.es>
2021-03-05 18:09:21 +0100

    2.0.7 - new files added

---
+++ R/gallery.R
@@ -0,0 +1,6 @@
+gallery <- function(nodes, name = NULL, label = NULL, color = NULL,
+    ntext = NULL, info = NULL, image = NULL,
+    zoom = 1, main = NULL, note = NULL, help = NULL,
+    language = c("en", "es", "ca"), dir = NULL){
+  return(gallery_rd3(nodes, name, label, color, ntext, info, image, zoom, main, note, help, language))
+}
