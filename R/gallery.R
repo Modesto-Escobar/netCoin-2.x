b191b67303eec21a59515ab92bd3436dc320fe89
BioinfoUSAL <bioinfo@usal.es>
2021-03-19 17:49:25 +0100

    2.0.8

--- R/gallery.R
+++ R/gallery.R
@@ -1,7 +1,8 @@
 gallery <- function(nodes, name = NULL, label = NULL, color = NULL,
     ntext = NULL, info = NULL, image = NULL,
     zoom = 1, main = NULL, note = NULL, help = NULL,
+    roundedItems = FALSE,
     language = c("en", "es", "ca"), dir = NULL){
   color <- setAttrByValueKey("color",color,nodes)
-  return(gallery_rd3(nodes, name, label, color, ntext, info, image, zoom, main, note, help, language))
+  return(gallery_rd3(nodes, name, label, color, ntext, info, image, zoom, main, note, help, roundedItems, language, dir))
 }
