8a5d3cedfaad400e8f2aa3a0702b5dcc1de275b3
BioinfoUSAL <bioinfo@usal.es>
2021-04-12 09:08:48 +0200

    added description argument in gallery

--- R/gallery.R
+++ R/gallery.R
@@ -1,12 +1,14 @@
 gallery <- function(nodes, name = NULL, label = NULL, color = NULL,
     ntext = NULL, info = NULL, image = NULL,
     zoom = 1, main = NULL, note = NULL, showLegend = TRUE,
-    help = NULL, helpOn = FALSE, roundedItems = FALSE,
+    help = NULL, helpOn = FALSE, description = NULL,
+    roundedItems = FALSE,
     language = c("en", "es", "ca"), dir = NULL){
   color <- setAttrByValueKey("color",color,nodes)
   return(gallery_rd3(nodes, name = name, label = label, color = color,
                      ntext = ntext, info = info, image = image,
                      zoom = zoom, main = main, note = note, showLegend = showLegend,
-                     help = help, helpOn = helpOn, roundedItems = roundedItems,
+                     help = help, helpOn = helpOn, description = description,
+                     roundedItems = roundedItems,
                      language = language, dir = dir))
 }
