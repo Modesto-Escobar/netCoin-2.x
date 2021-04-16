765d233723ee50d3c234d15e8ed8cc60d480b016
BioinfoUSAL <bioinfo@usal.es>
2021-04-16 17:51:58 +0200

    2.0.11

--- R/gallery.R
+++ R/gallery.R
@@ -1,14 +1,16 @@
 gallery <- function(nodes, name = NULL, label = NULL, color = NULL,
     ntext = NULL, info = NULL, image = NULL,
-    zoom = 1, main = NULL, note = NULL, showLegend = TRUE,
+    zoom = 1, itemsPerRow = NULL,
+    main = NULL, note = NULL, showLegend = TRUE,
     help = NULL, helpOn = FALSE, description = NULL,
-    roundedItems = FALSE,
+    descriptionWidth = NULL, roundedItems = FALSE,
     language = c("en", "es", "ca"), dir = NULL){
   color <- setAttrByValueKey("color",color,nodes)
   return(gallery_rd3(nodes, name = name, label = label, color = color,
                      ntext = ntext, info = info, image = image,
-                     zoom = zoom, main = main, note = note, showLegend = showLegend,
+                     zoom = zoom, itemsPerRow = itemsPerRow,
+                     main = main, note = note, showLegend = showLegend,
                      help = help, helpOn = helpOn, description = description,
-                     roundedItems = roundedItems,
+                     descriptionWidth = descriptionWidth, roundedItems = roundedItems,
                      language = language, dir = dir))
 }
